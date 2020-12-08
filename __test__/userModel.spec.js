d99949ce2468457c8294a2ecb3f315d057744da5
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-08 13:29:54 -0500

    working on developing tests

---
+++ __test__/userModel.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const server = require('../server.js');
+
+describe('GET /api/users should return an array of users', () => {
+    it('should return an array of objects', async () => { 
+        const result = await request(server).get('/api/users');
+        expect(result.body.users).toBe([{}]);
+    })  
+})
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,4 +1,4 @@
 const knex = require('knex');
 const config = require('../knexfile.js');
-const db = knex(config.development);
-module.exports = db;
+const dbEnv = process.env.DB_ENV || 'development';
+module.exports = knex(config[dbEnv]);
