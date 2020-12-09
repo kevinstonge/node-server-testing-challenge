88e959197728270ed53fa4b004e6a01316619c3d
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-09 08:16:04 -0500

    didn't have cross-env installed

--- __test__/userModel.spec.js
+++ __test__/userModel.spec.js
@@ -4,6 +4,6 @@ const server = require('../server.js');
 describe('GET /api/users should return an array of users', () => {
     it('should return an array of objects', async () => { 
         const result = await request(server).get('/api/users');
-        expect(result.body.users).toBe([{}]);
+        expect(result.body.users).toBe([]);
     })  
 })
